703001c536d6129b1a9a48b8a72804eb3be9b278
ZhangJianQ <1808691334@qq.com>
2021-05-25 19:36:29 +0800

    add link menu and message components

---
+++ src/components/link.vue
@@ -0,0 +1,32 @@
+<template>
+  <a
+    :href="disabled ? '' : href"
+    :class="[
+      'el-link',
+      `el-link--${type}`,
+      {
+        'is-disabled': disabled,
+        'is-underline': underline && !disabled
+      }
+    ]"
+    v-bind="$attrs"
+  ></a>
+</template>
+
+<script>
+export default {
+  name: 'Link',
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    underline: {
+      type: Boolean,
+      defualt: true
+    },
+    disabled: Boolean,
+    href: String
+  }
+}
+</script>
